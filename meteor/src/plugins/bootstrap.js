d6d0447a201c9bf0627e5c6d1af708ca339bb891
illure <illure_work@outlook.com>
2018-08-31 16:29:18 +0800

    【meteor】移除jQuery、bootstrap3，添加bootstrap4,bootstrap-vue

--- meteor/src/plugins/bootstrap.js
+++ meteor/src/plugins/bootstrap.js
@@ -1,2 +1,8 @@
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min'
+
+Vue.use(BootstrapVue)
